ba8e92ccd3b30ee1ac12cc1c7a49c9798056dcf4
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-06-09 18:35:08 +0100

    refactored about section with functional components and hooks

--- src/components/About.js
+++ src/components/About.js
@@ -1,88 +1,62 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "../styles/about.css";
 
-class Form extends Component {
-  render() {
-    return(
-      <form className={this.props.className} onSubmit={this.props.onSubmit}>
-        <div className="text-area-container">
-          <label htmlFor="about">Tell us a little  bit about you</label>
-          <textarea name="about" maxLength={220}></textarea>
-        </div>
-        <div className="btn-container">
-          <button type="submit">Edit</button>
-        </div>
-      </form>
-    )
-  }
+function Form({ className, onSubmit }) {
+  return(
+    <form className={className} onSubmit={onSubmit}>
+      <div className="text-area-container">
+        <label htmlFor="about">Tell us a little  bit about you</label>
+        <textarea name="about" maxLength={220}></textarea>
+      </div>
+      <div className="btn-container">
+        <button type="submit">Edit</button>
+      </div>
+    </form>
+  )
 }
 
-export default class About extends Component {
-  constructor(prop) {
-    super(prop)
-    this.state = {
-      input: "",
-      value: "I'm a creative person who has worked as an art director for the last 3 years and as a studio lead for 2 years before that. I'm happy, friendly, and love a good challenge.",
-      formClass: "about-form",
-      editBtn: "about-edit-btn-container",
-    };
-    this.handleMouseOver = this.handleMouseOver.bind(this)
-    this.handleMouseLeave = this.handleMouseLeave.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+export default function About() {
+  const [value, setValue] = useState("I'm a creative person who has worked as an art director for the last 3 years and as a studio lead for 2 years before that. I'm happy, friendly, and love a good challenge.")
+  const [formClass, setFormClass] = useState("about-form")
+  const [editBtn, setEditBtn] = useState("about-edit-btn-container")
+
+  const handleMouseOver = () => {
+    setEditBtn("about-edit-btn-container visible")
   }
 
-  async handleMouseOver() {
-    await this.setState({
-      editBtn: "about-edit-btn-container visible"
-    })
+  const handleMouseLeave = () => {
+    setEditBtn("about-edit-btn-container") 
   }
 
-  async handleMouseLeave() {
-    await this.setState({
-      editBtn: "about-edit-btn-container"
-    })
-  }
-
-  handleClick() {
-    if (this.state.formClass === "about-form form-visible") {
-      this.setState({formClass: "about-form"})
+  const handleClick = async() => {
+    if (formClass === "about-form form-visible") {
+      await setFormClass("about-form")
       return
     }
-    this.setState({formClass: "about-form form-visible"})
+    await setFormClass("about-form form-visible")
   }
 
-  async handleSubmit(e) {
+ const handleSubmit = (e) => {
     e.preventDefault()
-    
-    await this.setState({
-      input: e.target.about.value
-    })
- 
-    await this.setState({
-      value: this.state.input
-    })
-
-    this.handleClick()
+    setValue(e.target.about.value)
+    handleClick() // to close form
   }
 
-  render () {
-    return (
-      <div 
-        className="about-section"
-        onMouseEnter={this.handleMouseOver} 
-        onMouseLeave={this.handleMouseLeave}>
-        <div className={ this.state.editBtn }
-          onClick={this.handleClick}>
-          <i className="fa-solid fa-edit"></i>
-        </div>
-        <h2 className="section-heading">About Me</h2>
-        <p className="about-text">{this.state.value}</p>
-        <Form 
-          onSubmit={this.handleSubmit} 
-          className={this.state.formClass} 
-        />
+  return (
+    <div 
+      className="about-section"
+      onMouseEnter={handleMouseOver} 
+      onMouseLeave={handleMouseLeave}>
+      <div className={ editBtn }
+        onClick={handleClick}>
+        <i className="fa-solid fa-edit"></i>
       </div>
-    )
-  }
+      <h2 className="section-heading">About Me</h2>
+      <p className="about-text">{value}</p>
+      <Form 
+        onSubmit={handleSubmit} 
+        className={formClass} 
+      />
+    </div>
+  )
 } 
